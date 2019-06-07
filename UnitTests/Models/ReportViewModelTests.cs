7947425f45022ddb78063caa2bd3d71e376921a6
Matthew Echert <mgechert@gmail.com>
2019-06-06 20:04:48 -0700

    Added ReportViewModel Get Default test

--- UnitTests/Models/ReportViewModelTests.cs
+++ UnitTests/Models/ReportViewModelTests.cs
@@ -18,5 +18,19 @@ namespace UnitTests.Models
             // Assert
             Assert.IsNotNull(myTest);
         }
+
+        [TestMethod]
+        public void ReportViewModel_Get_Default_Should_Pass()
+        {
+            // Arrange
+            var myTest = new ReportViewModel();
+
+            // Act
+            var result = myTest.NumberOfUsers;
+
+            // Assert
+            Assert.IsNotNull(myTest.NumberOfUsers);
+        }
+
     }
 }
